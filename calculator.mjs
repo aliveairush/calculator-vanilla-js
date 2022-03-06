0c5d0d41b58670d4c1602a3640a060e1f00e9293
Aimurzin Ainur <aliveairush@gmail.com>
2022-03-06 20:34:32 +0300

    Fixed bugs and updated css styles.

--- calculator.mjs
+++ calculator.mjs
@@ -19,38 +19,51 @@ const peek = (stack) => stack[stack.length - 1];
 
 const isANumber = (record) => /\d+\.\d+|\d+/.test(record);
 
+// Splitting math expression on math chunks,
+// 1) Take if in the beginning is negative numbers with period 2)  Take if in the beginning is negative  numbers 3) Take positive numbers with period 4) Take Positive numbers 5) Take parenthesis and math signs
+const splitOnMathChunks = (string) =>  string.match(/^-\d+\.\d+|^-\d+|\d+\.\d+|\d+|[()+-/*//]/g);
+
 export default class Calculator {
-  #mathExpressionString;
   #calculatorHtmlElem
-  #calculatorOutputHtmlElem
+  #inputTextHtmlElem
+  #outputTextHtmlElem
+
+  #mathExpressionString;
   #numberStack;
   #operationStack;
   #buttons;
 
   #appendCharToMathExpression(number) {
     this.#mathExpressionString += number
-    this.#calculatorOutputHtmlElem.textContent = this.#mathExpressionString;
+    this.#inputTextHtmlElem.textContent = this.#mathExpressionString;
+  }
+
+
+  #updateMathExpression(expression) {
+    this.#inputTextHtmlElem.textContent = this.#mathExpressionString = expression;
   }
 
   #deleteLastCharFromMathExpression = () => {
-    this.#calculatorOutputHtmlElem.textContent = this.#mathExpressionString = this.#mathExpressionString.slice(0, -1);
-  }
-
-  #updateMathExpression(expression) {
-    this.#calculatorOutputHtmlElem.textContent = this.#mathExpressionString = expression;
+    // Если был введен 1 символ, то после его стирания нужно установить дефолт в 0
+    if (this.#mathExpressionString.length === 1) {
+      this.#updateMathExpression(DIGITS[0]);
+    } else { // Иначе просто передаем новую строку на 1 символ короче
+      this.#updateMathExpression(this.#mathExpressionString.slice(0, -1));
+    }
   }
 
   #prevCharacter() {
     return this.#mathExpressionString[this.#mathExpressionString.length - 1] || '';
   }
 
-  #handleClickGeneral = (btn) => {
-    if (btn.possiblePrevValues.includes(this.#prevCharacter())) {
-      this.#appendCharToMathExpression(btn.value);
+  #handleBtnClick = (btn) => {
+    // Если текущий символ не допустим (например * не может быть после +), то выбрасываем ошибку
+    const previousChar = this.#prevCharacter();
+    if (!btn.possiblePrevValues.includes(previousChar)){
+      throw new Error(`${btn.value} cant be used after ${previousChar}`);
     }
-    else {
-      console.log("ERROR")
-    }
+
+    this.#appendCharToMathExpression(btn.value);
   }
 
   #executeLastOperation = () => {
@@ -59,18 +72,13 @@ export default class Calculator {
     const operationSymbol = this.#operationStack.pop(); // Последняя операция -+*/
     const mathFunc = this.#buttons.get(operationSymbol).mathFunction // Получаем нужную функцию по символу
     const result = mathFunc(firstOperand, secondOperand); // Вычисление и сохранение результата
-    this.#numberStack.push(result); // Добавляем вычисленное значение обратно в стек чисел
+    this.#numberStack.push(result.toString()); // Добавляем вычисленное значение обратно в стек чисел
   }
 
   #executeAllOperationsInStack = () => {
     while (this.#operationStack.length > 0) {
       this.#executeLastOperation();
     }
-  }
-
-  log = () => {
-    console.log("Operation stack", this.#operationStack);
-    console.log("DIgig stack", this.#numberStack);
   }
 
   #executeOperationsInStackUntilLeftBracket = () => {
@@ -103,23 +111,47 @@ export default class Calculator {
     this.#operationStack.push(btn.value);
   }
 
+  #outputResultOfMathExpression = (result) => {
+    // TODO refactor
+    this.#outputTextHtmlElem.insertAdjacentHTML("beforeend", `<span>${this.#mathExpressionString} = ${result}</span>`)
+    const calcHeaderHtml = this.#calculatorHtmlElem.querySelector('.calc__item-header');
+    calcHeaderHtml.scrollTop = this.#outputTextHtmlElem.offsetHeight;
+  }
+
+  #btnOperationObject = ({value, level, mathFunction}) => {
+    return {
+      value,
+      level,
+      possiblePrevValues: [...DIGITS, RIGHT_BRACKET],
+      handleClick: this.#handleBtnClick,
+      mathFunction,
+      handleExecution: this.#handleOperationRecord,
+    }
+  }
+
   constructor() {
-    this.#mathExpressionString = EMPTY_STRING;
+    this.#mathExpressionString = DIGITS[0];
     this.#calculatorHtmlElem = document.querySelector('.calculator');
-    this.#calculatorOutputHtmlElem = this.#calculatorHtmlElem.querySelector(".calc__item-input");
+    this.#inputTextHtmlElem = this.#calculatorHtmlElem.querySelector(".calc__item-input");
+    this.#outputTextHtmlElem = this.#calculatorHtmlElem.querySelector(".calc__item-output");
     this.#operationStack = [];
     this.#numberStack = [];
 
-
+    // Event listener будет отлавливать все клики по кнопкам
     this.#calculatorHtmlElem.addEventListener('click', (e) => {
-      const clickedBtn = this.#buttons.get(e.target.dataset.content);
-      if (clickedBtn) clickedBtn.handleClick(clickedBtn);
+      try {
+        const clickedBtn = this.#buttons.get(e.target.dataset.content); // если data-content != null значит это кнопка
+        if (clickedBtn) clickedBtn.handleClick(clickedBtn); // Выполняем функцию по нажатию на кнопку
+      } catch (error){
+        console.warn(error.message);
+      }
     });
 
 
+    // Таблица из кнопок и соответствующих им функций по нажатию на кнопку
     this.#buttons = new Map(Object.entries({
         [CLEAR]: {
-          handleClick: this.#updateMathExpression.bind(this, EMPTY_STRING),
+          handleClick: this.#updateMathExpression.bind(this, DIGITS[0]),
         },
         [BACKSPACE]: {
           handleClick: this.#deleteLastCharFromMathExpression,
@@ -128,86 +160,71 @@ export default class Calculator {
           value: LEFT_BRACKET,
           possiblePrevValues: [EMPTY_STRING, ...OPERATORS, LEFT_BRACKET],
           level: 0,
-          handleClick: this.#handleClickGeneral,
-          handleExecution: () => this.#operationStack.push(LEFT_BRACKET)
+          handleClick: this.#handleBtnClick,
+          handleExecution: () => this.#operationStack.push(LEFT_BRACKET),
         },
         [RIGHT_BRACKET]: {
           value: RIGHT_BRACKET,
           possiblePrevValues: [...DIGITS, RIGHT_BRACKET],
-          handleClick: this.#handleClickGeneral,
+          handleClick: this.#handleBtnClick,
           handleExecution: this.#executeOperationsInStackUntilLeftBracket,
         },
         [PERIOD]: {
           value: PERIOD,
           possiblePrevValues: [...DIGITS],
           handleClick: (char) => {
+            // TODO refactor
             // Если последнее число уже содержит точку (.)
             if (/\d+\.\d+$/g.test(this.#mathExpressionString)) console.error("Error");
-            else this.#handleClickGeneral.call(this, char);
+            else this.#handleBtnClick.call(this, char);
           }
         },
-        [PLUS]: {
+        [PLUS]: this.#btnOperationObject({
           value: PLUS,
           level: 1,
-          possiblePrevValues: [...DIGITS, RIGHT_BRACKET],
-          handleClick: this.#handleClickGeneral,
-          mathFunction: (a, b) => {
-            console.log(`Math operation ${a} + ${b};`)
-            return a + b;
-          },
-          handleExecution: this.#handleOperationRecord,
-        },
-        [MINUS]: {
+          mathFunction: (a, b) => a + b
+        }),
+        [MINUS]: this.#btnOperationObject({
           value: MINUS,
           level: 1,
-          handleClick: this.#handleClickGeneral,
-          possiblePrevValues: [...DIGITS, RIGHT_BRACKET],
-          mathFunction: (a, b) => {
-            console.log(`Math operation ${a} - ${b};`)
-            return a - b;
-          },
-          handleExecution: this.#handleOperationRecord,
-        },
-        [MULTIPLICATION]: {
+          mathFunction: (a, b) => a - b,
+        }),
+        [MULTIPLICATION]: this.#btnOperationObject({
           value: MULTIPLICATION,
           level: 2,
-          handleClick: this.#handleClickGeneral,
-          possiblePrevValues: [...DIGITS, RIGHT_BRACKET],
-          mathFunction: (a, b) => {
-            console.log(`Math operation ${a} * ${b};`)
-            return a * b;
-          },
-          handleExecution: this.#handleOperationRecord,
-        },
-        [DIVISION]: {
+          mathFunction: (a, b) => a * b,
+        }),
+        [DIVISION]: this.#btnOperationObject({
           value: DIVISION,
           level: 2,
-          handleClick: this.#handleClickGeneral,
-          possiblePrevValues: [...DIGITS, RIGHT_BRACKET],
-          mathFunction: (a, b) => {
-            console.log(`Math operation ${a} / ${b};`)
-            return a / b;
-          },
-          handleExecution: this.#handleOperationRecord,
-        },
+          mathFunction: (a, b) => a / b,
+        }),
+
         [EQUATION]: {
+          // TODO when have only One digit in expression show warning
+          // TODO Make try catch
           handleClick: () => {
-            // const mockExpression = '2.2-1.1'
-            // const mathRecord = mockExpression.match(/[()+-/*//]|\d+\.\d+|\d+/g);
-            const mathRecord = this.#mathExpressionString.match(/[()+-/*//]|\d+\.\d+|\d+/g);
+            try {
+              const mathChunks = splitOnMathChunks(this.#mathExpressionString);
 
-            for (const record of mathRecord) {
-              if (isANumber(record)) { // Если число добавляем в стек чисел
-                this.#numberStack.push(record);
+              for (const chunk of mathChunks) {
+                if (isANumber(chunk)) { // Если число добавляем в стек чисел
+                  this.#numberStack.push(chunk);
+                }
+                else { // Иначе значит операция, выполнить операцию
+                  const btn = this.#buttons.get(chunk);
+                  btn.handleExecution(btn);
+                }
               }
-              else { // Иначе значит операция, выполнить операцию
-                const btn = this.#buttons.get(record);
-                btn.handleExecution(btn);
-              }
+              this.#executeAllOperationsInStack(); // Выполнить все оставшиеся операции
+
+              const finalResult = this.#numberStack.pop(); // В стеке останется одно число = ответ
+
+              this.#outputResultOfMathExpression(finalResult);
+              this.#updateMathExpression(finalResult);
+            } catch (error) {
+              this.#outputResultOfMathExpression('ERROR');
             }
-            this.#executeAllOperationsInStack(); // Выполнить все оставшиеся операции
-
-            alert(this.#numberStack.pop()) // В стеке останется одно число = ответ
           }
         },
       }
@@ -216,7 +233,13 @@ export default class Calculator {
     DIGITS.forEach(digit => this.#buttons.set(digit, {
       value: digit,
       possiblePrevValues: [EMPTY_STRING, ...DIGITS, ...OPERATORS, PERIOD, LEFT_BRACKET],
-      handleClick: this.#handleClickGeneral,
+      handleClick: () => {
+        // Если в строке только один 0, то мы должны его переписать
+        if (this.#mathExpressionString === DIGITS[0]){
+          this.#mathExpressionString = EMPTY_STRING;
+        }
+        this.#handleBtnClick(this.#buttons.get(digit));
+      },
     }));
   }
 }
