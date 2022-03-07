a249243b7af40649618c6c4abebb14c5c5c943b1
Aimurzin Ainur <aliveairush@gmail.com>
2022-03-07 20:10:18 +0300

    Did not touch any code, just rewrote comments from russian to english.

--- calculator.mjs
+++ calculator.mjs
@@ -32,49 +32,59 @@ export default class Calculator {
       throw new Error("Container cannot be null or undefined")
     }
 
-    this.#calculatorHtmlElem = this.#renderCalcHtml(container);
+    this.#calculatorHtmlElem = this.#renderCalcHtml(container); // Render calculator html
 
-    this.#inputString = DIGITS[0];
-    this.#inputTextHtmlElem = this.#calculatorHtmlElem.querySelector(".calc__item-input");
-    this.#outputTextHtmlElem = this.#calculatorHtmlElem.querySelector(".calc__item-output");
-    this.#operationStack = [];
-    this.#numberStack = [];
+    this.#inputString = DIGITS[0]; // Input string value by default is '0'
+    this.#inputTextHtmlElem = this.#calculatorHtmlElem.querySelector(".calc__item-input"); // DOM element where clicked button's values are displayed
+    this.#outputTextHtmlElem = this.#calculatorHtmlElem.querySelector(".calc__item-output"); // DOM element where math expression result is displayed
+    this.#operationStack = []; // Stack of operations such as "=-*/(
+    this.#numberStack = []; // Or in other words Operator stack
 
-    // Event listener будет отлавливать все клики по кнопкам
+    // Map initialization of the calculator button objects
+    this.#calcButtons = this.#calcButtonInitialization();
+
+    // Event listener on calc itself to catch button click using event bubbling
     this.#calculatorHtmlElem.addEventListener('click', (e) => {
       try {
-        const clickedBtn = this.#calcButtons.get(e.target.dataset.content);
-        if (clickedBtn) { // если data-content != null значит это кнопка
-          clickedBtn.handleClick(clickedBtn); // Выполняем функцию по нажатию на кнопку
+        const clickedBtn = this.#calcButtons.get(e.target.dataset.content); // Getting btn object by clicked character
+        if (clickedBtn) { // If data-content != null that means clicked object is a btn
+          clickedBtn.handleClick(clickedBtn); // Handle button click
         }
       } catch (error) {
         console.warn(error.message);
       }
     });
-
-    // Таблица из кнопок и соответствующих им функций по нажатию на кнопку
-    this.#calcButtons = this.#calcButtonInitialization();
   }
 
+  /** Every calculator button object at least has handleClick() function, which is being executed when user clicks on corresponded calculator button
+   Other optional properties:
+   - value: character of the button
+   - possiblePrevValues: this property helps to check if the current button value is allowed to be put in.
+   - level: priority of operation for example if we write 2+2*2 we have to execute multiplication first, that means multiplication should have higher priority
+   - mathFunction(): Only operation objects (/*+-) have this function.
+   - handleExecution(): Only operation objects and parentheses have this function. This func determines how operation should be executed
+   */
   #calcButtonInitialization = () => {
     const map = new Map(Object.entries({
         [CLEAR]: {
-          handleClick: this.#updateMathExpression.bind(this, DIGITS[0]),
+          handleClick: this.#updateMathExpression.bind(this, DIGITS[0]), // On "clear" button click input field value should be set as default value (0)
         },
         [BACKSPACE]: {
-          handleClick: this.#deleteLastCharFromMathExpression,
+          handleClick: this.#deleteLastCharFromMathExpression, // Removing last character from input field
         },
         [LEFT_BRACKET]: {
           value: LEFT_BRACKET,
           possiblePrevValues: [EMPTY_STRING, ...OPERATORS, LEFT_BRACKET],
-          level: 0,
+          level: 0, // Lowest priority
           handleClick: () => {
+            // If the input field contains only the one character, and it is 0, then we should clear 0 from the input field
             if (this.#inputString === DIGITS[0]) {
               this.#inputString = EMPTY_STRING;
             }
+            // Execute the standard button click using call method
             this.#handleBtnClick.call(this, this.#calcButtons.get(LEFT_BRACKET));
           },
-          handleExecution: () => this.#operationStack.push(LEFT_BRACKET),
+          handleExecution: () => this.#operationStack.push(LEFT_BRACKET), // Left bracket is special, we need to put it in the operation stack
         },
         [RIGHT_BRACKET]: {
           value: RIGHT_BRACKET,
@@ -86,7 +96,10 @@ export default class Calculator {
           value: PERIOD,
           possiblePrevValues: [...DIGITS],
           handleClick: () => {
-            if (doesLastNumberContainPeriod(this.#inputString)) throw new Error("There cannot be 2 dots in one number");
+            // Number should not have 2 dots
+            if (doesLastNumberContainPeriod(this.#inputString)) {
+              throw new Error("There cannot be 2 dots in one number");
+            }
             this.#handleBtnClick.call(this, this.#calcButtons.get(PERIOD));
           }
         },
@@ -114,23 +127,26 @@ export default class Calculator {
         [EQUATION]: {
           handleClick: () => {
             try {
-              const mathChunks = splitOnMathChunks(this.#inputString);
+
+              const mathChunks = splitOnMathChunks(this.#inputString); // Math chunks looks like ["2.2", "-", "4", "*", "2"]
 
               for (const chunk of mathChunks) {
-                if (isANumber(chunk)) { // Если число добавляем в стек чисел
+                if (isANumber(chunk)) { // If math chunk is a number, then put it in on top of numbers stack
                   this.#numberStack.push(chunk);
                 }
-                else { // Иначе значит операция, выполнить операцию
+                // Otherwise, chunk must be an operation, so get it's btn object and call handleExecution() function
+                else {
                   const btn = this.#calcButtons.get(chunk);
                   btn.handleExecution(btn);
                 }
               }
-              this.#executeAllOperationsInStack(); // Выполнить все оставшиеся операции
 
-              const finalResult = this.#numberStack.pop(); // В стеке останется одно число = ответ
+              this.#executeAllOperationsInStack(); // Execute all left operations in operation stack
 
-              this.#displayMathResult(finalResult);
-              this.#updateMathExpression(finalResult);
+              const finalResult = this.#numberStack.pop(); // After operations execution, number stack should have only one item in it and it is the answer
+
+              this.#displayMathResult(finalResult); // Display answer into output html element
+              this.#updateMathExpression(finalResult); // Set new value input field value as the previous expression result
             } catch (error) {
               this.#displayMathResult('ERROR');
             }
@@ -143,7 +159,7 @@ export default class Calculator {
       value: digit,
       possiblePrevValues: [EMPTY_STRING, ...DIGITS, ...OPERATORS, PERIOD, LEFT_BRACKET],
       handleClick: () => {
-        // Если в строке только один 0, то мы должны его переписать
+        // If input field value contains only 0, then on digit btn click it is better to get rid of 0
         if (this.#inputString === DIGITS[0]) {
           this.#inputString = EMPTY_STRING;
         }
@@ -154,16 +170,18 @@ export default class Calculator {
     return map;
   }
 
+  // Function updates input field text content and inner string variable with given string
   #updateMathExpression(expression) {
     this.#inputTextHtmlElem.textContent = this.#inputString = expression;
   }
 
   #deleteLastCharFromMathExpression = () => {
-    // Если был введен 1 символ, то после его стирания нужно установить дефолт в 0
+    // If input field has only 1 symbol then set input field value by default as 0
     if (this.#inputString.length === 1) {
       this.#updateMathExpression(DIGITS[0]);
     }
-    else { // Иначе просто передаем новую строку на 1 символ короче
+    // Else just shorten input field value by 1 symbol
+    else {
       this.#updateMathExpression(this.#inputString.slice(0, -1));
     }
   }
@@ -171,8 +189,8 @@ export default class Calculator {
   #prevCharacter = () => this.#inputString[this.#inputString.length - 1] || EMPTY_STRING;
 
   #handleBtnClick = (btn) => {
-    // Если текущий символ не допустим (например * не может быть после +), то выбрасываем ошибку
     const previousChar = this.#prevCharacter();
+    // If current symbol is not allowed after previous symbol (e.g. * cant go after +), then throw an Error
     if (!btn.possiblePrevValues.includes(previousChar)) {
       throw new Error(`${btn.value} cant be used after ${previousChar}`);
     }
@@ -180,12 +198,13 @@ export default class Calculator {
   }
 
   #executeLastOperation = () => {
-    const secondOperand = Number(this.#numberStack.pop()); // Второй операнд - число
-    const firstOperand = Number(this.#numberStack.pop()); // Первый операнд - число
-    const operationSymbol = this.#operationStack.pop(); // Последняя операция -+*/
-    const mathFunc = this.#calcButtons.get(operationSymbol).mathFunction // Получаем нужную функцию по символу
-    const result = mathFunc(firstOperand, secondOperand); // Вычисление и сохранение результата
-    this.#numberStack.push(result.toString()); // Добавляем вычисленное значение обратно в стек чисел
+    const secondOperand = Number(this.#numberStack.pop());
+    const firstOperand = Number(this.#numberStack.pop());
+    const operationSymbol = this.#operationStack.pop();
+
+    const mathFunc = this.#calcButtons.get(operationSymbol).mathFunction // Get needed math function by operation symbol
+    const result = mathFunc(firstOperand, secondOperand); // Execute math function
+    this.#numberStack.push(result.toString()); // Put math result variable on top of the number stack
   }
 
   #executeAllOperationsInStack = () => {
@@ -195,37 +214,39 @@ export default class Calculator {
   }
 
   #executeOperationsInStackUntilLeftBracket = () => {
-    let prevOperation = peek(this.#operationStack); // Достаем операцию до скобки
+    let prevOperation = peek(this.#operationStack);  // Peeking last operation symbol from operation stack
 
-    while (prevOperation !== LEFT_BRACKET) { // Пока предыдущая операция не левая скобка
+    while (prevOperation !== LEFT_BRACKET) { // While last operation symbol is not left bracket execute last operation
       this.#executeLastOperation();
       prevOperation = peek(this.#operationStack);
     }
-    this.#operationStack.pop(); // Удаляем левую скобку
+    this.#operationStack.pop(); // Removing Left bracket from operation stack
   }
 
   /**
-   * Функция управляет решением как поступить с операцией.
+   * Function for operations such as ()/*-+
    */
-  #handleExecution = (btn) => {
+  #handleExecution = (operationBtn) => {
     const operationStack = this.#operationStack;
-    // Если стек операций пустой, то сразу добавляем операцию в стек и выходим из функции
+    // If operation stack is empty, then just add operation on top and exit the function
     if (isEmpty(operationStack)) {
-      operationStack.push(btn.value);
+      operationStack.push(operationBtn.value);
       return
     }
 
-    let prevBtn = this.#calcButtons.get(peek(operationStack)); // Достаем прошлую операцию из стека
+    // Peeking last operation object from operation stack
+    let prevBtn = this.#calcButtons.get(peek(operationStack));
 
-    while (prevBtn && btn.level <= prevBtn.level) { // Выполнение операции пока не останется операций или текущая операция не будет выше в приоритете предыдущей
+    // While there is an operation in operation stack and while current operation priority is lower or equal to previous btn object
+    while (prevBtn && operationBtn.level <= prevBtn.level) {
       this.#executeLastOperation();
-      prevBtn = this.#calcButtons.get(peek(this.#operationStack));
+      prevBtn = this.#calcButtons.get(peek(this.#operationStack)); // After the last operation executed take new last operation
     }
-    this.#operationStack.push(btn.value);
+    this.#operationStack.push(operationBtn.value); // Put current operation object on top of operation stack
   }
 
   /**
-   * Функция отображает результат вычисления
+   * Function displays math result in output dom element
    */
   #displayMathResult = (result) => {
     this.#outputTextHtmlElem.insertAdjacentHTML("beforeend", `<span>${this.#inputString} = ${result}</span>`)
@@ -233,6 +254,11 @@ export default class Calculator {
     calcHeaderHtml.scrollTop = this.#outputTextHtmlElem.offsetHeight;
   }
 
+  /**
+   * Function to create operation button objects
+   * All operation have one 3 properties in common: possiblePrevValues, handleClick(), handleExecution()
+   * And operation can have different value, level and math function itself.
+   */
   #btnOperationObject = ({value, level, mathFunction}) => {
     return {
       value,
@@ -244,6 +270,7 @@ export default class Calculator {
     }
   }
 
+  // Simple rendering calculator html in container dom element.
   #renderCalcHtml = (container) => {
     const calc = document.createElement('div');
     calc.className = 'calculator';
